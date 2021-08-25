c36a9138a71a34aa7a946b1d421ba70741d321f8
dohoanggiahuy317 <dohoanggiahuy@Dos-MacBook-Pro.local>
2021-08-25 23:32:58 +0700

    ok back end

--- add_data.js
+++ add_data.js
@@ -2,17 +2,29 @@
 var db = firebase.firestore()
 
 function add_data(){
+    var user_name = document.getElementById("appointment_name").value;
+    var user_department = document.getElementById("appointment_department").value;
+    var user_email = document.getElementById("appointment_email").value;
+    var user_date = document.getElementById("appointment_date").value;
+    var user_time = document.getElementById("appointment_time").value;
+    var user_phone = document.getElementById("appointment_phone").value;
+
     // Add a new document in collection "cities"
-    db.collection("cities").doc("LA").set({
-        name: "Los Angeles",
-        state: "CA",
-        country: "USA"
+    db.collection("med schedule").doc(user_email).set({
+        department: user_department,
+        name: user_name,
+        date: user_date,
+        time:user_time,
+        phone: user_phone,
     })
     .then(() => {
         console.log("Document successfully written!");
+        window.location.replace('index.html');
     })
     .catch((error) => {
         console.error("Error writing document: ", error);
+        
     });
-}
 
+    
+}
