aeaaa4f5a4655f1415cf5dcac4436f6cbd6b8415
dohoanggiahuy317 <dohoanggiahuy@Dos-MacBook-Pro.local>
2021-08-26 23:18:00 +0700

    loader done

--- js/main.js
+++ js/main.js
@@ -29,15 +29,7 @@
 	};
 	fullHeight();
 
-	// loader
-	var loader = function() {
-		setTimeout(function() { 
-			if($('#ftco-loader').length > 0) {
-				$('#ftco-loader').removeClass('show');
-			}
-		}, 1);
-	};
-	loader();
+
 
 	// Scrollax
    $.Scrollax();
